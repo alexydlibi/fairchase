4f782b06f7475af5837a89def7c667d7364f5234
Alex Ydlibi <alexydlibi@gmail.com>
2023-04-11 21:44:16 +0100

    ready1

--- script.js
+++
@@ -1,7 +0,0 @@
-const toggleButton = document.getElementById("toggle-button");
-const box2Content = document.querySelector(".box3-content");
-
-toggleButton.addEventListener("click", function() {
-  box2Content.classList.toggle("hidden");
-  box2Content.classList.toggle("visible");
-});
---
+++ src_fc/sendmail.php
@@ -0,0 +1,11 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $to = "info@fairchase.co.uk";
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $headers = "From: " . $_POST['name'] . " <" . $_POST['email'] . ">";
+
+    mail($to, $subject, $message, $headers);
+    echo "Email sent successfully.";
+}
+?>
